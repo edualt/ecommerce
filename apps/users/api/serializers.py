426d06189d15587b1f35276398030c7d8c221ad3
edualt <eduardosebastianhdz503@gmail.com>
2022-06-30 19:31:19 -0500

    feat(users): users api

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -4,11 +4,15 @@ from apps.users.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'first_name', 'last_name']
-        extra_kwargs = { 'password' : { 'write_only': True } }
-
+        fields = ['id', 'email', 'first_name', 'last_name',]
+        fields = ['id', 'email', 'first_name', 'last_name', 'password']
+        extra_kwargs = {
+            'password' : {
+                'write_only' : True
+            }
+        }
     def create(self, validated_data):
-        user = User(
+        user = User.objects.create(
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
--- apps/users/api/viewsets.py
+++ apps/users/api/viewsets.py
@@ -1,13 +1,13 @@
 from rest_framework import viewsets, status
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
 class UserViewset(viewsets.ModelViewSet):
-    serializer = UserSerializer
-    queryset = User.objects.all
-    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
 
     # def list(self, request):
     #     users = self.get_queryset()
