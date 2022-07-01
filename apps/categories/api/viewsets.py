d07787ea7d5411d323f90d339632c18ca115c360
edualt <eduardosebastianhdz503@gmail.com>
2022-07-01 17:43:15 -0500

    feat(categories)

--- apps/categories/api/viewsets.py
+++ apps/categories/api/viewsets.py
@@ -1,4 +1,3 @@
-from gzip import READ
 from rest_framework import viewsets, status
 from apps.categories.api.serializers import CategorySerializer
 from apps.categories.models import Category
@@ -9,7 +8,7 @@ from rest_framework.response import Response
 class CategoryViewset(viewsets.ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
-    #permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def list(self, request):
         categories = Category.objects.all()
@@ -21,5 +20,11 @@ class CategoryViewset(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({'message':'Category added'}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # def destroy(self, request):
+    #     permission_classes = [IsAdminUser]
+    #     instance = self.get_object()
+    #     self.perform_destroy(instance)
+    #     return Response(status=status.HTTP_200_OK)
