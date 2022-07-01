3b7a87d5e74f0a7d864dbd2c9164f9d64536704e
edualt <eduardosebastianhdz503@gmail.com>
2022-07-01 17:43:35 -0500

    feat(users)

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from apps.users.models import User
+from apps.categories.models import Category
+from apps.products.models import Product
 
 admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Product)
--- apps/users/api/viewsets.py
+++ apps/users/api/viewsets.py
@@ -1,15 +1,26 @@
 from rest_framework import viewsets, status
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, DjangoModelPermissions
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
+from rest_framework.response import Response
 
 class UserViewset(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [DjangoModelPermissions]
 
 
-    # def list(self, request):
-    #     users = self.get_queryset()
-    #     user_serializer = self.list_serializer_class(users, many=True)
-    #     return Response(user_serializer.data, status=status.HTTP_200_OK)
+    def list(self, request):
+        permission_classes = [DjangoModelPermissions.authenticated_users_only]
+        queryset = User.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        permission_classes = [DjangoModelPermissions.has_permission]
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User was created successfully'},  status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+             
